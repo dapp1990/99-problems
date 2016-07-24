40cbfa5abcce61219880bb9888e1bddfa54fc31f
Daniel Andrés Pérez Pérez <dapp.1990@gmail.com>
2016-07-24 23:05:55 +0200

    p33 completed using prolog

--- prolog/p33.pl
+++ prolog/p33.pl
@@ -1,8 +1,9 @@
-% Determine whether two positive integer numbers are coprime.
+% Calculate Euler's totient function phi(m).
 % Two numbers are coprime if their greatest common divisor equals 1.
 
 %Problem P33
-:- op(700,xfx,'gcd').
+:- op(500,fx,totient_phi).
+:- op(100,xfx,'gcd').
 
 gcd(N,N,N).
 gcd(N1,N2,GCD) :-
@@ -16,3 +17,20 @@ gcd(N1,N2,GCD) :-
     ).
 
 coprime(N1,N2) :- gcd(N1,N2,1).
+
+totient_phi(1,1).
+totient_phi(Number,Phi) :- 
+    Number > 1,
+    totient_phi_3(Number,1,Phi).
+
+totient_phi_3(Num,Num,0).
+totient_phi_3(Num,Counter,Phi) :-
+    Num > Counter,
+	(   coprime(Num,Counter) ->
+	    Phi2 is 1
+	    ;   
+		Phi2 is 0
+    ),
+    Counter1 is Counter + 1,
+    totient_phi_3(Num,Counter1,Phi1),
+    Phi is Phi1 + Phi2.    
